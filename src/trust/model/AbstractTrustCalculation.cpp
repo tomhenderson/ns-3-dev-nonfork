3292a8aed96a21b996e8b97320cf354565dffbb7
judeniroshan <jude.niroshan11@gmail.com>
2018-05-24 01:40:09 +0530

    new trust framework module created. Abstract trust table is implemented

---
+++ src/trust/model/AbstractTrustCalculation.cpp
@@ -0,0 +1,22 @@
+/*
+ * AbstractTrustCalculation.cpp
+ *
+ *  Created on: May 24, 2018
+ *      Author: jude
+ */
+
+#include "AbstractTrustCalculation.h"
+#include "ns3/ipv4-address.h"
+
+namespace ns3 {
+
+AbstractTrustCalculation::AbstractTrustCalculation() {
+	// TODO Auto-generated constructor stub
+
+}
+
+AbstractTrustCalculation::~AbstractTrustCalculation() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace ns3 */
